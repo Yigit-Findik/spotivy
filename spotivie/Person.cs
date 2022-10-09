4bd987212512c35aef786388038114f5748fca95
Yigit <71589865+Yigit-Findik@users.noreply.github.com>
2022-10-09 21:43:02 +0200

    Added some classes for skeleton app just need superuser and client class to finish whole skeleton

---
+++ spotivie/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spotivie
+{
+
+    //NEED REVIEW UNSURE IF RIGHT
+
+    internal class Person
+    {
+        public string Name;
+        private List<Person> Friends;
+        private List<Playlist> Playlists;
+
+        public Person(string name, List<Person> friends, List<Playlist> playlists)
+        {
+            Name = name;
+            Friends = friends;
+            Playlists = playlists;
+        }
+
+        public List<Person> ShowFriends()
+        {
+            return Friends;
+        }
+
+        public List<Playlist> ShowPlaylists()
+        {
+            return Playlists;
+        }
+
+        //inherits playlist??? ask Robert for more info
+        public Playlist SelectPlaylist(int hit)
+        {
+            return Playlists[hit];
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ spotivie/Playlist.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spotivie
+{
+    internal class Playlist : SongCollection
+    {
+        public Person Owner;
+
+        public Playlist(string title, Person owner) : base(title)
+        {
+            Owner = owner;
+        }
+
+        public void Add(iPlayable playable)
+        {
+            //empty for now
+        }
+
+        public void Remove(iPlayable playable)
+        {
+            //empty for now
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
